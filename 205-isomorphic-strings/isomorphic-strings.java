c031f64928d2e5029239fcdac8dfbc5a2610ebe7
Hema1910 <139840445+Hema1910@users.noreply.github.com>
2024-08-04 22:03:34 +0530

    Time: 27 ms (6.62%) | Memory: 44.3 MB (6.12%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+      if (s.length() != t.length()) return false;
+        
+        HashMap<Character, Integer> mapS = new HashMap<>();
+        HashMap<Character, Integer> mapT = new HashMap<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char charS = s.charAt(i);
+            char charT = t.charAt(i);
+            
+            if (mapS.containsKey(charS) && !mapT.containsKey(charT)) return false;
+            if (!mapS.containsKey(charS) && mapT.containsKey(charT)) return false;
+            
+            if (!mapS.containsKey(charS) && !mapT.containsKey(charT)) {
+                mapS.put(charS, i);
+                mapT.put(charT, i);
+            } else {
+                if (!mapS.get(charS).equals(mapT.get(charT))) return false;
+                mapS.put(charS, i);
+                mapT.put(charT, i);
+            }
+        }
+        return true;  
+    }
+}
