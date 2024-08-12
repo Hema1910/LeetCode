6c5fa6abed5c7245a6fc99fc0e8b532e6b2c4a4b
Hema1910 <139840445+Hema1910@users.noreply.github.com>
2024-08-12 19:56:10 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (34.91%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -1,29 +1,29 @@
 public class Solution {
     public ListNode reverseBetween(ListNode head, int m, int n) {
-        if (head == null) return null;
+        if (head == null)
+        { return null;
+        }
 
-        // Create a dummy node to handle edge cases where m = 1
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
+        ListNode temp = new ListNode(0);
+        temp.next = head;
 
-        // Move the pointer to the node just before the m-th node
-        ListNode prev = dummy;
+
+        ListNode prev = temp;
         for (int i = 1; i < m; i++) {
             prev = prev.next;
         }
 
-        // The m-th node becomes the tail of the reversed sublist
-        ListNode start = prev.next;
-        ListNode then = start.next;
+        ListNode fast = prev.next;
+        ListNode slow= fast.next;
 
-        // Reverse the sublist from m to n
+      
         for (int i = 0; i < n - m; i++) {
-            start.next = then.next;
-            then.next = prev.next;
-            prev.next = then;
-            then = start.next;
+            fast.next = slow.next;
+            slow.next = prev.next;
+            prev.next = slow;
+            slow = fast.next;
         }
 
-        return dummy.next;
+        return temp.next;
     }
 }
