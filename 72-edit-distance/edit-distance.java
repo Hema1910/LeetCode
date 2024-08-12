94c6648cb00e6570caba88925d401f7bcb3a4791
Hema1910 <139840445+Hema1910@users.noreply.github.com>
2024-08-12 20:17:52 +0530

    Time: 5 ms (66.30%) | Memory: 44.5 MB (94.32%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -3,22 +3,21 @@ class Solution {
         int m = word1.length();
         int n = word2.length();
         
-        // Create a 2D array to store the distances
         int[][] dp = new int[m + 1][n + 1];
         
-        // Initialize the dp array
+   
         for (int i = 0; i <= m; i++) {
             for (int j = 0; j <= n; j++) {
                 if (i == 0) {
-                    dp[i][j] = j; // If word1 is empty, insert all characters of word2
+                    dp[i][j] = j; 
                 } else if (j == 0) {
-                    dp[i][j] = i; // If word2 is empty, remove all characters of word1
+                    dp[i][j] = i; 
                 } else if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1]; // No change needed if characters are the same
+                    dp[i][j] = dp[i - 1][j - 1]; 
                 } else {
-                    dp[i][j] = 1 + Math.min(dp[i - 1][j],        // Remove
-                                           Math.min(dp[i][j - 1],    // Insert
-                                                    dp[i - 1][j - 1])); // Replace
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j],        
+                                           Math.min(dp[i][j - 1],    
+                                                    dp[i - 1][j - 1])); 
                 }
             }
         }
