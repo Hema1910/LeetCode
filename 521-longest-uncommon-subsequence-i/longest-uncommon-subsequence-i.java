23a8fe343529694ee23ef17582983c32c0738e98
Hema1910 <139840445+Hema1910@users.noreply.github.com>
2024-08-13 19:50:06 +0530

    Time: 4 ms (100.00%) | Memory: 41.6 MB (9.63%) - LeetSync

--- 521-longest-uncommon-subsequence-i/longest-uncommon-subsequence-i.java
+++ 521-longest-uncommon-subsequence-i/longest-uncommon-subsequence-i.java
@@ -1,9 +1,38 @@
 class Solution {
     public int findLUSlength(String a, String b) {
-        if (a.equals(b)) {
-            return -1;
+    
+        int m=a.length();
+        int n=b.length();
+
+        int dp[][]=new int[m+1][n+1];
+
+        for(int i=0;i<m+1;i++)
+        {
+            for(int j=0;j<n+1;j++)
+            {
+                dp[i][j]=0;
+            }
         }
-       
-        return Math.max(a.length(), b.length());
+
+        for(int i=1;i<m+1;i++)
+        {
+            for(int j=1;j<n+1;j++)
+            {
+                if(a.charAt(i-1)==b.charAt(j-1))
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+        }
+         if(dp[m][n]==m &&m==n)
+         {
+             return -1;
+         }
+
+         return Math.max(m,n);
     }
 }
