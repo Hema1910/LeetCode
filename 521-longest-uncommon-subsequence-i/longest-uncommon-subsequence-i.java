74d5dd86f321d3dc865ee2e416ccdf6ea22dfb64
Hema1910 <139840445+Hema1910@users.noreply.github.com>
2024-08-13 19:53:09 +0530

    Time: 4 ms (100.00%) | Memory: 41.5 MB (16.53%) - LeetSync

--- 521-longest-uncommon-subsequence-i/longest-uncommon-subsequence-i.java
+++ 521-longest-uncommon-subsequence-i/longest-uncommon-subsequence-i.java
@@ -4,13 +4,13 @@ class Solution {
         int m=a.length();
         int n=b.length();
 
-        int dp[][]=new int[m+1][n+1];
+        int arr[][]=new int[m+1][n+1];
 
         for(int i=0;i<m+1;i++)
         {
             for(int j=0;j<n+1;j++)
             {
-                dp[i][j]=0;
+                arr[i][j]=0;
             }
         }
 
@@ -20,15 +20,15 @@ class Solution {
             {
                 if(a.charAt(i-1)==b.charAt(j-1))
                 {
-                    dp[i][j]=1+dp[i-1][j-1];
+                    arr[i][j]=1+arr[i-1][j-1];
                 }
                 else
                 {
-                    dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
+                    arr[i][j]=Math.max(arr[i][j-1],arr[i-1][j]);
                 }
             }
         }
-         if(dp[m][n]==m &&m==n)
+         if(arr[m][n]==m &&m==n)
          {
              return -1;
          }
