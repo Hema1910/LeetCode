1d1e3c032892ec88ccb28a98ac4d0468854481f9
Hema1910 <139840445+Hema1910@users.noreply.github.com>
2024-08-17 10:47:44 +0530

    Time: 8 ms (61.04%) | Memory: 45.2 MB (15.34%) - LeetSync

---
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -0,0 +1,37 @@
+class Solution {
+   
+    public static String shortestPalindrome(String s) {
+        
+        String rev = new StringBuilder(s).reverse().toString();
+        
+     
+        String combined = s + "#" + rev;
+        
+  
+        int[] prefix = computePrefix(combined);
+     
+        int palindromeLength = prefix[prefix.length - 1];
+        
+        return rev.substring(0, s.length() - palindromeLength) + s;
+    }
+
+    private static int[] computePrefix(String s) {
+        int[] prefix = new int[s.length()];
+        int j = 0;
+        
+        for (int i = 1; i < s.length(); i++) {
+            while (j > 0 && s.charAt(i) != s.charAt(j)) {
+                j = prefix[j - 1];
+            }
+            if (s.charAt(i) == s.charAt(j)) {
+                j++;
+            }
+            prefix[i] = j;
+        }
+        return prefix;
+    }
+
+   
+}
+
+    
