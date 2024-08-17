c0221a84c1bc8f0cffb878279d3a9f29d1fc718d
Hema1910 <139840445+Hema1910@users.noreply.github.com>
2024-08-17 14:46:25 +0530

    Time: 618 ms (11.65%) | Memory: 45.1 MB (22.01%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.java
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -1,28 +1,37 @@
 class Solution {
-   
-    public static String shortestPalindrome(String s) {
-        String rev = new StringBuilder(s).reverse().toString();
-        String combined = s + "#" + rev;
-        int[] prefix = computePrefix(combined);
-        int palindromeLength = prefix[prefix.length - 1];
-        return rev.substring(0, s.length() - palindromeLength) + s;
-    }
 
-    private static int[] computePrefix(String s) {
-        int[] prefix = new int[s.length()];
-        int j = 0;
-        
-        for (int i = 1; i < s.length(); i++) {
-            while (j > 0 && s.charAt(i) != s.charAt(j)) {
-                j = prefix[j - 1];
+    public boolean isPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
             }
-            if (s.charAt(i) == s.charAt(j)) {
-                j++;
-            }
-            prefix[i] = j;
+            i++;
+            j--;
         }
-        return prefix;
+        return true;
     }
 
-}
+    public String shortestPalindrome(String s) {
 
+        if (s.length() <= 1) {
+            return s;
+        }
+
+   
+        int longestPalindromeEnd = 0;
+        for (int i = s.length(); i >= 0; i--) {
+            if (isPalindrome(s.substring(0, i))) {
+                longestPalindromeEnd = i;
+                break;
+            }
+        }
+
+       
+        String suffix = s.substring(longestPalindromeEnd);
+        String reversedSuffix = new StringBuilder(suffix).reverse().toString();
+
+       
+        return reversedSuffix + s;
+    }
+}
