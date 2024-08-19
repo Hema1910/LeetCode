075e303eff67e8cad203069f74c5c65adda07ab8
Hema1910 <139840445+Hema1910@users.noreply.github.com>
2024-08-19 20:32:27 +0530

    Time: 3 ms (56.72%) | Memory: 42.1 MB (63.70%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+public class Solution {
+    
+    public static String makeGood(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (!stack.isEmpty() && Math.abs(stack.peek() - c) == 32) {
+                stack.pop(); 
+            } else {
+                stack.push(c);
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for (char c : stack) {
+            result.append(c);
+        }
+        
+        return result.toString();
+    }
+
+}
