227c04bb4c816537edeeabf8df5ea0fbc834a7e9
Hema1910 <139840445+Hema1910@users.noreply.github.com>
2024-08-19 14:08:06 +0530

    Time: 0 ms (100.00%) | Memory: 45.5 MB (35.72%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,21 +1,15 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ones = 0;   // To keep track of numbers that have appeared once
-        int twos = 0;   // To keep track of numbers that have appeared twice
-        int threes = 0; // To keep track of numbers that have appeared three times
-
+        int ones = 0;   
+        int twos = 0;   
+        int threes = 0; 
         for (int num : nums) {
-            // Update `twos` with bits that were previously in `ones` and now appear again
             twos |= ones & num;
-            // Update `ones` with current number
             ones ^= num;
-            // Calculate `threes` where `ones` and `twos` have bits set
             threes = ones & twos;
-            // Remove `threes` from `ones` and `twos` since the numbers appearing three times should be discarded
             ones &= ~threes;
             twos &= ~threes;
         }
-
-        return ones; // `ones` will hold the number that appears exactly once
+        return ones;
     }
 }
