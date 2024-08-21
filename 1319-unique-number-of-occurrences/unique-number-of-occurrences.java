b39a164f5d48eb4fe95faa79e1c8f6f86f01f0e2
Hema1910 <139840445+Hema1910@users.noreply.github.com>
2024-08-21 15:12:14 +0530

    Time: 2 ms (98.05%) | Memory: 42.2 MB (14.61%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        HashSet<Integer> set=new HashSet<>();
+        for(int element :arr){
+            map.put(element,map.getOrDefault(element,0)+1);
+        }
+        for (int count : map.values()) {
+            if (!set.add(count)) {
+                return false;
+            }
+        }
+        return true;   
+    }
+}
